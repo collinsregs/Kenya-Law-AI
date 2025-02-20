65483bbd057dbed3f364655744adf90488aa6370
collins iregi <collins@farwell-consultants.com>
2025-02-20 16:12:42 +0300

    supreme court cases

--- constitution.py
+++ constitution.py
@@ -16,6 +16,7 @@ load_dotenv()
 # Get the BASE_URL from the environment variables
 BASE_URL = os.getenv("BASE_URL")
 
+global_section_index = 1
 constitutionUrl = "/akn/ke/act/2010/constitution/eng@2010-09-03";
 chapters=["ONE","Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten","Eleven","TWELVE","Thirteen","Fourteen","Fifteen","Sixteen","Seventeen","Eighteen"] #check exact case incase of case sensitivity
 
@@ -49,29 +50,51 @@ def getPreamble(preamble_container):
 
 def getChapter(driver):
     chapters_object={}
+    number="1"
     for chapter in chapters:
         try:
+
             chapter_class =f"chp_{chapter}"
+            print(chapter_class)
             chp_container=driver.find_element(By.ID,chapter_class)
             title = chp_container.find_element(By.TAG_NAME,'h2').text
-            sections = getSec(chapter_container=chp_container, chapter_class=chapter_class)
-            chapters_object[f"CHAPTER {chapter}"]={"title":title,"sections":sections}
+            try:
+                
+                if chapter=="TWELVE" :
+                    number ="I"
+                print(f"TRY: {chapter_class}__part_{number}")
+                chp_container.find_element(By.ID,f"{chapter_class}__part_{number}")
+                parts = getPart(chp_container, section_class=chapter_class)
+                chapters_object[f"CHAPTER {chapter}"]={"title":title,"sections":parts}
+                number ="1"
+            except:
+                sections = getSec(chapter_container=chp_container, chapter_class=chapter_class, starting_index=global_section_index)
+                chapters_object[f"CHAPTER {chapter}"]={"title":title,"sections":sections}
         except Exception as e:
-            print(f"Error Retrieving chapter {chapter}",e)
+            print(f"")
     return chapters_object
 
-def getSec(chapter_container, chapter_class):
+def getSec(chapter_container, chapter_class,starting_index):
+    global global_section_index
     section_object={}
-    index =1
+    index =starting_index
     while True:
         try:
             section_class =chapter_class+"__sec_"+f"{index}"
+            print(section_class)
             sec_container = chapter_container.find_element(By.ID,section_class)
             title = sec_container.find_element(By.TAG_NAME,'h3').text
-            subsections = getSubSec(section_container=sec_container, section_class=section_class)
-            section_object[f"Section {index}"]={"title":title,"sub_sections":subsections}
+            try:
+                sec_container.find_element(By.ID,f"{section_class}__subsec_1")
+                subsections = getSubSec(section_container=sec_container, section_class=section_class)
+                section_object[f"Section {index}"]={"title":title,"sub_sections":subsections}
+            except:
+                content = getContent(sec_container,section_class)
+                section_object[f"Section {index}"]={"title":title,"Content":content}
             index=index+1
-        except:
+        except Exception as e:
+            global_section_index =  index
+            print(f"exception in get section")
             break
 
     return section_object
@@ -82,24 +105,55 @@ def getSubSec(section_container,section_class):
     while True:
         try:
             sub_section_class =section_class+"__subsec_"+f"{index}"
+            print (sub_section_class)
             sub_sec_container = section_container.find_element(By.ID,sub_section_class)
             title = sub_sec_container.find_element(By.CLASS_NAME,'akn-num').text
-            content = getContent(subSectionContainer=sub_sec_container, section_class=sub_section_class)
+            content = getContent(subSectionContainer=sub_sec_container, subSectionClass=sub_section_class)
             sub_section_object[f"Subsection {index}"]={"number":title,"sub_sections":content}
             index=index+1
-        except:
+            
+        except Exception as e:
+            # print(f"exception in get section")
+            break
+
+    return sub_section_object
+
+
+def getPart(section_container,section_class):
+    sub_section_object={}
+    index =1
+    while True:
+        try:
+            sub_section_class =section_class+"__part_"+f"{index}"
+            if section_class=="chp_TWELVE" and index==1:
+                    sub_section_class =section_class+"__part_"+f"I"
+            print ("part",sub_section_class)
+            sub_sec_container = section_container.find_element(By.ID,sub_section_class)
+            title = sub_sec_container.find_element(By.TAG_NAME,'h2').text
+            content = getSec(sub_sec_container,sub_section_class,global_section_index)
+            sub_section_object[f"Part {index}"]={"title":title,"Sections":content}
+            index=index+1
+
+        except Exception as e:
+            # print(f"exception in get section")
             break
 
     return sub_section_object
 
 def getContent(subSectionContainer, subSectionClass):
+
+    # print("getting content for:",subSectionClass)
     content ={}
     try:
-        point=subSectionContainer.find_element(By.CLASS_NAME,'akn-content')
-        content["point"] =point.text
-    except:
         content['intro'] =subSectionContainer.find_element(By.CLASS_NAME,'akn-intro').text
         content['paragraphs']= getPara(subSectionContainer,subSectionClass)
+
+        # print("getting actual content",point.text)
+    except:
+        point=subSectionContainer.find_element(By.CLASS_NAME,'akn-content')
+        content["point"] =point.text
+        # print("getting paragraph",point.text)
+
     return content
 
 def getPara(subSectionContainer,subSectionClass):
@@ -125,7 +179,7 @@ def main():
     driver = webdriver.Chrome()
     driver.get(url)
     content = getChapter(driver=driver)
-    print(content)
+    # print(content)
 
 
     time.sleep(5)
